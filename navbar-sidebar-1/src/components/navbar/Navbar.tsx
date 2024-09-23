d1f8868c71c6102e446cead5c4523e195fe6c36c
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-09-23 14:22:02 +0530

    [responsive] navbar

--- navbar-sidebar-1/src/components/navbar/Navbar.tsx
+++ navbar-sidebar-1/src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react';
 import styles from "./Navbar.module.scss";
+import Link from 'next/link';
 
 type Props = {}
 
@@ -8,8 +9,20 @@ const Navbar = (props: Props) => {
   
   return (
     <>
+      <div className={styles.backdrop}></div>
       <header className={styles.navbar}>
+        <span className={`material-symbols-outlined ${styles['menu-icon']}`}>menu</span>
         <h1>Navbar</h1>
+        <nav className={styles.navbar__links}>
+          <Link href="/">Home</Link>
+          <Link href="/">Insert</Link>
+          <Link href="/">Design</Link>
+          <Link href="/">Layout</Link>
+          <Link href="/">References</Link>
+          <Link href="/">Mailings</Link>
+          <Link href="/">Review</Link>
+          <Link href="/">View</Link>
+        </nav>
       </header>
     </>
   )
