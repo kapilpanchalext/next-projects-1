373b83ad7f763544a9b98f309d6b8d966026c839
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-09-30 11:14:31 +0530

    [tsup.config.ts] all styles in page.css

--- navbar-sidebar-1/src/app/page.tsx
+++ navbar-sidebar-1/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { HomeFramework } from "@kapilpanchal/helloworld-framework-1";
 import '@kapilpanchal/helloworld-framework-1/dist/index.css';
-import "@kapilpanchal/helloworld-framework-1/dist/app.css";
 
 export default function Home() {
   return (
